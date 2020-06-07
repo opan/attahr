cb788eb2a48993aeb76b0d383aa44d8a97d74bf2
opanmustopah <opan.mustopah@go-jek.com>
2020-06-07 14:03:26 +0700

    fix: remove pry line

--- config/initializers/authentication.rb
+++ config/initializers/authentication.rb
@@ -2,7 +2,6 @@ require 'warden'
 
 Warden::Strategies.add(:password) do
   def authenticate!
-    pry
     params.fetch('user', {})['email'] == "opan@mail.com" ? success!(params['user']['email']) : fail!('Wrong email')
   end
 end
