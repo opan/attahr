d580ba43efdffd60e5982706e3d9235eedbb2efd
opanmustopah <opan.neutron@gmail.com>
2020-08-20 15:11:55 +0700

    fix: Prevent authenticated user to access sign in and sign up page

--- apps/web/controllers/sessions/new.rb
+++ apps/web/controllers/sessions/new.rb
@@ -3,8 +3,12 @@ module Web
     module Sessions
       class New
         include Web::Action
+        include Web::Authentication
 
         def call(params)
+          if authenticated?
+            redirect_to routes.root_path
+          end
         end
       end
     end
--- apps/web/controllers/users/sign_up.rb
+++ apps/web/controllers/users/sign_up.rb
@@ -3,11 +3,16 @@ module Web
     module Users
       class SignUp
         include Web::Action
+        include Web::Authentication
 
         expose :user
 
         def call(params)
           @user = User.new
+
+          if authenticated?
+            redirect_to routes.root_path
+          end
         end
       end
     end
