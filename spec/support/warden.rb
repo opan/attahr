ea324bbf8e806200050742e982e627a95ef6b1c9
opanmustopah <opan.neutron@gmail.com>
2020-08-21 15:24:33 +0700

    fix: Unit test controllers

--- spec/support/warden.rb
+++ spec/support/warden.rb
@@ -1,6 +1,6 @@
 RSpec.configure do |config|
   config.before(:each) do
-    @warden = double("Warden", authenticate: true, authenticate!: true)
+    @warden = double("Warden", authenticate: true, authenticate!: true, authenticated?: true, user: 'current_user')
   end
 end
 
--- spec/web/controllers/sessions/new_spec.rb
+++ spec/web/controllers/sessions/new_spec.rb
@@ -1,6 +1,10 @@
 RSpec.describe Web::Controllers::Sessions::New, type: :action do
   let(:action) { described_class.new }
-  let(:params) { Hash[] }
+  let(:params) { {'warden' => double('Warden', user: 'current_user')} }
+
+  before(:each) do
+    allow(action).to receive(:authenticated?).and_return(false)
+  end
 
   it 'is successful' do
     response = action.call(params)
