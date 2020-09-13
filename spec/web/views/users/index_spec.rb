01df7b8ebb7fcd5dc8a824193b3b8ed1315ea818
opanmustopah <opan.neutron@gmail.com>
2020-09-13 19:43:58 +0700

    fix: spec view missing required exposures

--- spec/web/views/users/index_spec.rb
+++ spec/web/views/users/index_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe Web::Views::Users::Index, type: :view do
   let(:users) {
     [double('user', id: 1, username: 'user-1', email: 'user-1@mail.com')]
   }
-  let(:exposures) { Hash[format: :html, users: users, params: {}, flash: {}] }
+  let(:exposures) { Hash[format: :html, users: users, params: {}, flash: {}, current_user: 'current_user'] }
   let(:template)  { Hanami::View::Template.new('apps/web/templates/users/index.html.erb') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
