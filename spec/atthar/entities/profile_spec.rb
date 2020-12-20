747f52c762d12dafe5e605e4d6131225e3571de1
opanmustopah <opan.neutron@gmail.com>
2020-12-20 13:24:22 +0700

    test: add spec user and profile entity

--- spec/atthar/entities/profile_spec.rb
+++ spec/atthar/entities/profile_spec.rb
@@ -1,3 +1,9 @@
 RSpec.describe Profile, type: :entity do
-  # place your tests here
+  it 'has attributes' do
+    expect( subject.respond_to?(:id) ).to be true
+    expect( subject.respond_to?(:name) ).to be true
+    expect( subject.respond_to?(:dob) ).to be true
+    expect( subject.respond_to?(:created_at )).to be true
+    expect( subject.respond_to?(:updated_at) ).to be true
+  end
 end
--- spec/atthar/entities/user_spec.rb
+++ spec/atthar/entities/user_spec.rb
@@ -1,3 +1,12 @@
 RSpec.describe User, type: :entity do
-  # place your tests here
+  it 'has attributes' do
+    expect( subject.respond_to?(:id) ).to be true
+    expect( subject.respond_to?(:username) ).to be true
+    expect( subject.respond_to?(:email) ).to be true
+    expect( subject.respond_to?(:password_hash) ).to be true
+    expect( subject.respond_to?(:created_at )).to be true
+    expect( subject.respond_to?(:updated_at) ).to be true
+
+    expect( subject.respond_to?(:profile) ).to be true
+  end
 end
