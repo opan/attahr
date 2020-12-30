50aa1f711efe4a65f7ebc1231c5fd88f1499f679
opanmustopah <opan.neutron@gmail.com>
2020-12-30 13:49:36 +0700

    update sign up form

--- apps/web/views/helpers.rb
+++ apps/web/views/helpers.rb
@@ -4,7 +4,7 @@ module Web
       def sign_out_link
         unless current_user.nil?
           form_for :session, routes.sign_out_path, method: :delete do
-            submit 'Sign Out', class: 'mr-4 lg:inline-block text-sm px-4 py-2 leading-none border font-bold rounded text-blue-300 border-blue-300 hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0'
+            submit 'Sign Out', class: 'mr-4 lg:inline-block text-sm px-4 py-2 leading-none border font-bold rounded text-blue-500 border-blue-500 hover:border-blue-500 hover:text-white hover:bg-blue-500 mt-4 lg:mt-0'
           end
         end
       end
