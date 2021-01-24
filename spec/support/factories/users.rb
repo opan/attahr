aebc005053d779e6696a662d7093a6b7b9094865
opanmustopah <opan.neutron@gmail.com>
2021-01-24 08:10:23 +0700

    add factories user admin

--- spec/support/factories/users.rb
+++ spec/support/factories/users.rb
@@ -5,10 +5,7 @@ Factory.define(:user) do |f|
   f.timestamps
 end
 
-Factory.define(:user_admin) do |f|
-  f.sequence(:email) { |n| "admin#{n}@mail.com" }
-  f.sequence(:username) { |n| "admin-username#{n}" }
-  f.password_hash { BCrypt::Password.create('123') }
+Factory.define(user_admin: :user) do |f|
   f.superadmin true
   f.timestamps
 end
