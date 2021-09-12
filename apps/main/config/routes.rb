52c821dd87c7d0d5ef17a283423b836fdb822f7f
opanmustopah <opan.neutron@gmail.com>
2021-09-12 11:39:04 +0700

    chore: update routes org members

--- apps/main/config/routes.rb
+++ apps/main/config/routes.rb
@@ -8,5 +8,8 @@ get '/sign_up', to: 'users#sign_up', as: 'sign_up'
 post '/sign_up', to: 'users#register', as: 'register_user'
 get '/unauthenticated', to: 'sessions#new'
 
-resources :orgs, except: [:destroy]
-get '/orgs/:id/members', to: 'orgs#members', as: 'org_members'
+resources :orgs, except: [:destroy] do
+  member do
+    get :members
+  end
+end
