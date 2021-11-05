802c71536b8dab4128b050552cd9108f3d098ffb
opan <opan.neutron@gmail.com>
2021-11-05 15:01:51 +0700

    chore: fix landing spec

--- spec/main/controllers/landing/index_spec.rb
+++ spec/main/controllers/landing/index_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe Main::Controllers::Landing::Index, type: :action do
   let(:action) { described_class.new }
-  let(:params) { Hash[] }
+  let(:params) { Hash['warden' => @warden] }
 
   it 'is successful' do
     response = action.call(params)
