5e9908dd6ddae77a35fd4faa7122e414750b305f
opanmustopah <opan.mustopah@gojek.com>
2022-06-30 15:11:19 +0700

    add pos#updated_qty api

--- apps/main/controllers/point_of_sales/update_qty.rb
+++ apps/main/controllers/point_of_sales/update_qty.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 module Main
   module Controllers
     module PointOfSales
@@ -10,12 +11,52 @@ module Main
 
         before :authenticate!
 
-        def initialize()
-          
+        params do
+          required(:trx_id).filled
+          required(:trx_item_id).filled
+          required(:updated_qty).filled(:int?)
+        end
+
+        def initialize(
+          pos_trx_repo: PosTrxRepository.new,
+          pos_trx_item_repo: PosTrxItemRepository.new
+        )
+          @pos_trx_repo = pos_trx_repo
+          @pos_trx_item_repo = pos_trx_item_repo
         end
 
         def call(params)
-          
+          unless params.valid?
+            self.body = error_messages(params.error_messages)
+            self.status = 400
+            return
+          end
+
+          pos_trx = @pos_trx_repo.find_by_trx_id(params[:trx_id])
+          if pos_trx.nil?
+            self.body = error_messages(["Invalid transaction ID #{params[:trx_id]}"])
+            self.status = 400
+            return
+          end
+
+          @pos_trx_repo.transaction do
+            pos_trx_item = @pos_trx_item_repo.find(params[:trx_item_id])
+            if pos_trx_item.nil? || pos_trx_item.pos_trx_id != pos_trx.id
+              self.body = error_messages(["Invalid transaction item #{params[:trx_item_id]}"])
+              self.status = 400
+              return
+            end
+
+            trx_item_entity = PosTrxItem.new(pos_trx_item.to_h.merge!({ qty: params[:updated_qty] }))
+            @pos_trx_item_repo.update(pos_trx_item.id, trx_item_entity)
+
+            trx_items = @pos_trx_item_repo.find_by_pos_trx(pos_trx.id)
+            self.body = JSON.generate(
+              {
+                trx_items: trx_items.map { |i| PosTrxItemList.new(i).to_h }
+              }
+            )
+          end
         end
 
         private
---
+++ spec/main/controllers/point_of_sales/update_qty_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+RSpec.describe Main::Controllers::PointOfSales::UpdateQty, type: :action do
+  let(:pos_trx_repo) { instance_double('PosTrxRepository') }
+  let(:pos_trx_item_repo) { instance_double('PosTrxItemRepository') }
+
+  let(:current_user) { @warden.user }
+  let(:pos_trx) { Factory.structs[:pos_trx] }
+  let(:pos_trx_item) { Factory.structs[:pos_trx_item, pos_trx_id: pos_trx.id] }
+  let(:action) {
+    described_class.new(
+      pos_trx_repo: pos_trx_repo,
+      pos_trx_item_repo: pos_trx_item_repo
+    )
+  }
+  let(:params) { Hash['warden' => @warden] }
+
+  context 'with basic user' do
+    context 'when all params are valid' do
+      before do
+        params.merge!({ trx_id: pos_trx.id, trx_item_id: pos_trx_item.id, updated_qty: 5 })
+        updated_trx_item = PosTrxItem.new(pos_trx_item.to_h.merge!({ qty: 5 }))
+
+        expect(pos_trx_repo).to receive(:find_by_trx_id).with(pos_trx.id).and_return(pos_trx)
+        expect(pos_trx_repo).to receive(:transaction).and_yield
+        expect(pos_trx_item_repo).to receive(:find).with(pos_trx_item.id).and_return(pos_trx_item)
+        expect(pos_trx_item_repo).to receive(:update).with(pos_trx_item.id, updated_trx_item)
+        expect(pos_trx_item_repo).to receive(:find_by_pos_trx).with(pos_trx.id).and_return([pos_trx_item])
+
+        @response = action.call(params)
+      end
+
+      it 'return 200' do
+        expect(@response[0]).to eq(200)
+      end
+
+      it 'return #trx_items' do
+        expect(JSON.parse(@response[2][0]).length).to eq 1
+      end
+    end
+  end
+end
