1f997382f981f770307f56eb16ddbb51b4215628
opanmustopah <opan.mustopah@gojek.com>
2022-07-01 13:51:08 +0700

    update pos#update_qty spec

--- spec/main/controllers/point_of_sales/update_qty_spec.rb
+++ spec/main/controllers/point_of_sales/update_qty_spec.rb
@@ -38,5 +38,43 @@ RSpec.describe Main::Controllers::PointOfSales::UpdateQty, type: :action do
         expect(JSON.parse(@response[2][0]).length).to eq 1
       end
     end
+
+    context 'when trx_id is invalid' do
+      before do
+        params.merge!({ trx_id: pos_trx.id, trx_item_id: pos_trx_item.id, updated_qty: 5 })
+
+        expect(pos_trx_repo).to receive(:find_by_trx_id).with(pos_trx.id).and_return(nil)
+
+        @response = action.call(params)
+      end
+
+      it 'return 400' do
+        expect(@response[0]).to eq(400)
+      end
+
+      it 'return #errors' do
+        expect(JSON.parse(@response[2][0])['errors']).to eq(["Invalid transaction ID #{pos_trx.id}"])
+      end
+    end
+
+    context 'when trx_item_id is invalid' do
+      before do
+        params.merge!({ trx_id: pos_trx.id, trx_item_id: pos_trx_item.id, updated_qty: 5 })
+
+        expect(pos_trx_repo).to receive(:find_by_trx_id).with(pos_trx.id).and_return(pos_trx)
+        expect(pos_trx_repo).to receive(:transaction).and_yield
+        expect(pos_trx_item_repo).to receive(:find).with(pos_trx_item.id).and_return(nil)
+
+        @response = action.call(params)
+      end
+
+      it 'return 400' do
+        expect(@response[0]).to eq(400)
+      end
+
+      it 'return #errors' do
+        expect(JSON.parse(@response[2][0])['errors']).to eq(["Invalid transaction item #{pos_trx_item.id}"])
+      end
+    end
   end
 end
